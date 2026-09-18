3fd5d8ebcc0e8563810a76776bf3cabebcd0701e
ani-4nirudh <praani@mailbox.org>
2026-09-18 17:50:35 +0200

    build(project): Add sources.mk for build generation
    Add this file which would be used by Makefile for build generation. Its
    sole purpose is to list the files needed to build this project.

--- project/sources.mk
+++ project/sources.mk
@@ -2,13 +2,12 @@
 
 # Step 1: Define directories
 BUILD_DIR := build
-SRC_DIR := src
-INC_DIR := include
+COMMON_DIR := common
+
+CORE_DIR := src
 LINKER_DIR := linker
-EXT_DIR := external
 
 # Step 2: Import files
 SOURCES := $(wildcard $(SRC_DIR)/*.c $(SRC_DIR)/*/*.c)
 STARTUP_SOURCE := $(wildcard $(SRC_DIR)/*/*.S)
-EXT_SOURCES := $(wildcard $(EXT_DIR)/*/*.c)
-INCLUDES := -I$(INC_DIR) -I$(EXT_DIR)/printf -I$(INC_DIR)/motor -I$(INC_DIR)/lcd -I$(INC_DIR)/temp
+INCLUDES := -I./cmsis -I./config -I./core/include -I./middleware/FreeRTOS/Sourc
