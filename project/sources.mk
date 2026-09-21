1aecac87741bede89bac40bacf903fc713e0356a
ani-4nirudh <praani@mailbox.org>
2026-09-21 15:51:05 +0200

    build(project): Makefile changes for easier builds
    The Makefile is able to successfully generate builds now.

--- project/sources.mk
+++ project/sources.mk
@@ -3,17 +3,20 @@
 # Step 1: Define directories
 BUILD_DIR := build
 COMMON_DIR := common
-SRC_DIR := ./core/source
+SRC_DIR := core/source
 LINKER_DIR := linker
 STARTUP_DIR := startup
-FREERTOS_DIR := ./middleware/FreeRTOS
+FREERTOS_DIR := middleware/FreeRTOS
 
 # Step 2: Import files
 FREERTOS_SOURCES := $(wildcard $(FREERTOS_DIR)/Source/*.c \
-															 $(FREERTOS_DIR)/portable/GCC/ARM_CM4F/*.c \
-															 $(FREERTOS_DIR)/portable/MemMang/*.c)
+															 $(FREERTOS_DIR)/Source/portable/GCC/ARM_CM4F/*.c \
+															 $(FREERTOS_DIR)/Source/portable/MemMang/*.c)
+
 STARTUP_SOURCE := $(wildcard $(STARTUP_DIR)/*.s)
+
 SOURCES := $(wildcard $(SRC_DIR)/*.c)
+SOURCES += $(FREERTOS_SOURCES)
 
 INCLUDES := -I./cmsis \
 						-I./config \
