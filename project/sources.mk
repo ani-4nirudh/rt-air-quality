b5e115d420c70a0d5fbb6510f9a864c044c53440
ani-4nirudh <praani@mailbox.org>
2026-09-22 12:09:35 +0200

    build: Change Makefile to build the project with the peripheral

--- project/sources.mk
+++ project/sources.mk
@@ -1,26 +1,34 @@
 # sources.mk
 
-# Step 1: Define directories
+# Define directories
 BUILD_DIR := build
 COMMON_DIR := common
 SRC_DIR := core/source
 LINKER_DIR := linker
 STARTUP_DIR := startup
 FREERTOS_DIR := middleware/FreeRTOS
+PERI_DIR := peripherals
 
-# Step 2: Import files
+# Add filepath for the startup file
+STARTUP_SOURCE := $(wildcard $(STARTUP_DIR)/*.s)
+
+# Add filepaths
 FREERTOS_SOURCES := $(wildcard $(FREERTOS_DIR)/Source/*.c \
 															 $(FREERTOS_DIR)/Source/portable/GCC/ARM_CM4F/*.c \
 															 $(FREERTOS_DIR)/Source/portable/MemMang/*.c)
 
-STARTUP_SOURCE := $(wildcard $(STARTUP_DIR)/*.s)
+# Add filepaths for peripherals
+PERI_SOURCES := $(wildcard $(PERI_DIR)/source/*.c)
 
+# Add them together as the files have the same '.c' extension
 SOURCES := $(wildcard $(SRC_DIR)/*.c)
-SOURCES += $(FREERTOS_SOURCES)
+SOURCES += $(FREERTOS_SOURCES) $(PERI_SOURCES)
 
+# Add include files
 INCLUDES := -I./cmsis \
 						-I./config \
 						-I./core/include \
 						-I./core/source \
+						-I./peripherals/include \
 						-I$(FREERTOS_DIR)/Source/portable/GCC/ARM_CM4F \
 						-I$(FREERTOS_DIR)/Source/include/ 
